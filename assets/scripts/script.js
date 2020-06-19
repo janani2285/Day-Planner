24ae6b75afb2420791cfa0ca399eb5fecfba0fb5
Janani NA <janani2285@gmail.com>
2020-06-18 21:31:57 -0700

    Added code for local storage and retrival

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,31 @@
-$(function(){
-    var now = moment();
-    var nowFormat = now.format("dddd, MMMM Do YYYY");
-    $("#currentDay").text(nowFormat);
+$(function () {
+    //Displays current date
+    var today = moment();
+    var todayFormat = today.format("dddd, MMMM Do YYYY");
+
+    // alert(today.isBefore(someDay));
+    $("#currentDay").text(todayFormat);
+
+    onLoad();
+
+    //Save button's onclick event
+    $(".saveBtn").on("click", function () {
+        var hour = $(this).parent().attr("id");
+        var desc = $(this).parent().children("textarea").val();
+        localStorage.setItem(hour, desc);
+    });
+
+    //Function to load the saved hourly activity from local storage.
+    function onLoad() {
+        for (var i = 9; i <= 17; i++) {
+            var lsKey = "hour-" + i;
+            var desc = localStorage.getItem(lsKey);
+            if (desc != null) {
+                var id = "#" + lsKey;
+                $(id).children("textarea").val(desc);
+            }
+
+        }
+
+    }
 });
